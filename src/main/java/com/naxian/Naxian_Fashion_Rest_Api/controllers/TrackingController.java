6259121ab6aed671ec0012648c857b5d26358e56
Riaj <riajmazumder711@gmail.com>
2023-05-29 14:54:51 +0600

    a

--- src/main/java/com/naxian/Naxian_Fashion_Rest_Api/controllers/TrackingController.java
+++ src/main/java/com/naxian/Naxian_Fashion_Rest_Api/controllers/TrackingController.java
@@ -2,6 +2,7 @@ package com.naxian.Naxian_Fashion_Rest_Api.controllers;
 
 
 import com.naxian.Naxian_Fashion_Rest_Api.models.TrackModel;
+import com.naxian.Naxian_Fashion_Rest_Api.services.EmailService;
 import com.naxian.Naxian_Fashion_Rest_Api.services.TrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,10 @@ public class TrackingController {
 
     @Autowired
     private TrackingService trackingService;
+
+
+    @Autowired
+    private EmailService emailService;
 
     @GetMapping("track/{id}")
     public TrackModel getAllByIdTrackingProduct(@PathVariable Long id){
