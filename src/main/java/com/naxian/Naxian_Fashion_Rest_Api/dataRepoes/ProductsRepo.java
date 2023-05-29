f77fc697561f55c5409675c6e6794b47ba3882dc
Riaj <riajmazumder711@gmail.com>
2023-05-29 21:07:29 +0600

    fixing search feature

--- src/main/java/com/naxian/Naxian_Fashion_Rest_Api/dataRepoes/ProductsRepo.java
+++ src/main/java/com/naxian/Naxian_Fashion_Rest_Api/dataRepoes/ProductsRepo.java
@@ -19,7 +19,7 @@ public interface ProductsRepo extends JpaRepository<Products, Long> {
     @Query(nativeQuery = true, value = "INSERT INTO Products (qAndA_aboutTheProduct) VALUES(:qAndA_aboutTheProductValue)")
     public void setProductsQAndA_AboutTheProduct(@Param("qAndA_aboutTheProductValue") List<QAndA_AboutTheProduct> qAndA_aboutTheProductValue);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM naxian_fashion.products where sub_cate = ?")
+    @Query(nativeQuery = true, value = "SELECT * FROM railway.products where sub_cate = ?")
     public List<Products> searchdProductsBySubCategory(String subCategory);
 
 
